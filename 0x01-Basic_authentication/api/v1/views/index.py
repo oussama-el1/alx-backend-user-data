cb06ed941f5f605ca0174324165f14fb575067cd
oussama-el1 <elhadraoui284@gmail.com>
2024-06-04 23:39:56 +0100

    401 status code

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+  """ 
+  unauthorized emdpoint
+  """
+
+  abort(401)
