04d4476c97174e6418cc891072a89609e7266391
oussama-el1 <elhadraoui284@gmail.com>
2024-06-05 01:13:18 +0100

    extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """
 Basic auth module
 """
-
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -10,3 +10,19 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        tmp = authorization_header
+
+        if tmp is None or not isinstance(tmp, str) or tmp[:6] != 'Basic ':
+            return None
+
+        return authorization_header[6:]
