deeb7a2b2130699d0224e6fd6aca67f78fd6decb
oussama-el1 <elhadraoui284@gmail.com>
2024-06-05 02:06:17 +0100

    solve

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -76,11 +76,11 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        users = User.search({"email": user_email})
-        if users == []:
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
             return None
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
-            else:
-                return None
+            return None
