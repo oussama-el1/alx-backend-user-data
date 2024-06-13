2dd68bf61ca686ff4f48aacabf8591befa3a083d
oussama-el1 <elhadraoui284@gmail.com>
2024-06-14 00:05:14 +0100

    .\app.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,8 +58,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
-            user.session_id = session_id
-
+            self._db.update_user(user_id=user.id, session_id=session_id)
             return session_id
         except Exception:
             return None
