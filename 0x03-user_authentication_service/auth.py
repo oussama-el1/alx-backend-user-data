a2f0b0586742ec46240f81bb1a52da06936fe843
oussama-el1 <elhadraoui284@gmail.com>
2024-06-13 17:58:55 +0100

    solve

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,5 +45,6 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode(), user.hashed_password)
-        except Exception:
+        except NoResultFound:
             return False
+        return False
