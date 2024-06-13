1d268c4f3845e7138a947c8017b528a0c853621a
oussama-el1 <elhadraoui284@gmail.com>
2024-06-14 00:55:53 +0100

    /profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,21 @@ def logout() -> str:
         abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """profile
+    """
+
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": user.email}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
