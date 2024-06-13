874a42eed6d07855048b1adabe59fed0fd1d82fa
oussama-el1 <elhadraoui284@gmail.com>
2024-06-13 16:40:43 +0100

    solve

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,7 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -31,7 +31,7 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """  register_user  """
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
             user = self._db.add_user(email=email,
                                      hashed_password=_hash_password(password))
