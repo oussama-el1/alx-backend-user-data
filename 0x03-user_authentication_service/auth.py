0688d8cb5f73a6930bec835a628be06134e5dbe1
oussama-el1 <elhadraoui284@gmail.com>
2024-06-14 01:42:23 +0100

    update pwd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -105,6 +105,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(user_id=user.id,
                                  hashed_password=_hash_password(password),
-                                 reset_token=reset_token)
+                                 reset_token=None)
         except Exception:
             raise ValueError("User Not Found")
