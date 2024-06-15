c507d28076ac25c218567c624f40075533dcf32d
oussama-el1 <elhadraoui284@gmail.com>
2024-06-15 19:51:36 +0100

    user id by sessios id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,27 @@ Class Session Auth
 """
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     SessionAuth Class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        :param user_id:
+        :type user_id:
+        :return:
+        :rtype:
+        """
+
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
