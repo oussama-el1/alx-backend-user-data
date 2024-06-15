35577a3d68468eac140bee9ab1fbf78e742db99a
oussama-el1 <elhadraoui284@gmail.com>
2024-06-15 20:15:09 +0100

    solve UUID to str data

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,7 +24,7 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
