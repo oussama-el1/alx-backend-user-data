fde5239cad49d96861235461e8165fb4de6b0625
oussama-el1 <elhadraoui284@gmail.com>
2024-06-15 21:09:55 +0100

    Solve

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -69,8 +69,9 @@ def before_request():
             if auth.current_user(request) is None:
                 abort(403)
 
-        if auth.authorization_header(request) and auth.session_cookie(request):
-            return None, abort(401)
+        if auth.authorization_header(request) is None\
+                and auth.session_cookie(request) is None:
+            abort(401)
 
 
 if __name__ == "__main__":
