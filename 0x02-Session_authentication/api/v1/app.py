0c881c630f7145cc272d1808c4c2f9d7f4bc38d8
oussama-el1 <elhadraoui284@gmail.com>
2024-06-16 13:46:11 +0100

    solve flask app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,6 +24,8 @@ elif os.environ.get("AUTH_TYPE") == 'session_auth':
     auth = SessionAuth()
 elif os.environ.get("AUTH_TYPE") == 'session_exp_auth':
     auth = SessionExpAuth()
+elif os.environ.get("AUTH_TYPE") == 'session_db_auth':
+    auth = SessionDBAuth()
 elif os.environ.get("AUTH_TYPE") == 'basic_auth':
     auth = BasicAuth()
 
