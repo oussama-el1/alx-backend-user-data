e44d93990dfab8d6deae4301ea9289e459e77e82
oussama-el1 <elhadraoui284@gmail.com>
2024-06-16 13:36:33 +0100

    last tas

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+SessionDBAuth class
+"""
+
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from datetime import timedelta, datetime
+
+
+class SessionDBAuth(SessionExpAuth):
+    """
+    SessionDBAuth class
+    """
+
+    def create_session(self, user_id=None):
+        """
+
+        :param user_id:
+        :type user_id:
+        :return:
+        :rtype:
+        """
+
+        session_id = super().create_session(user_id)
+        if session_id:
+            user_session = UserSession(user_id=user_id, session_id=session_id)
+            user_session.save()
+            user_session.save_to_file()
+        else:
+            return None
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        :param session_id:
+        :type session_id:
+        :return:
+        :rtype:
+        """
+
+        if session_id is None:
+            return None
+
+        users = UserSession.search({"session_id": session_id})
+        if not users:
+            return None
+
+        user = users[0]
+
+        expired_time = user.created_at + timedelta(seconds=self.session_duration)
+
+        if expired_time < datetime.utcnow():
+            return None
+
+        return user.user_id
+
+    def destroy_session(self, request=None):
+        """
+        :param request:
+        :type request:
+        :return:
+        :rtype:
+        """
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        user_session = UserSession.get(user_id)
+        try:
+            user_session.remove()
+            UserSession.save_to_file()
+        except Exception:
+            return False
+
+        return True
